cf2f4ef4d9973de4756159fa476a7d2ad8dd1ace
Jeroen Simonetti <jsimonetti@users.noreply.github.com>
2020-08-20 09:15:53 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,8 +10,8 @@ if [ "$1" = 'dovecot' ]; then
     set +e
     deluser vmail
     delgroup vmail
-    addgroup -g $PGID vmail && adduser -u $PUID -G vmail -h /var/vmail -D -s /sbin/nologin vmail
     set -e
+    addgroup -g $PGID vmail && adduser -u $PUID -G vmail -h /var/vmail -D -s /sbin/nologin vmail
     mkdir -p /var/vmail/conf.d /var/vmail/auth.d
     chown -R vmail:vmail /var/vmail/conf.d
     chown -R dovecot:dovecot /var/vmail/auth.d/
